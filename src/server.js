478d5d49c8549f777ba579ea702f06104945abbc
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-28 22:56:54 +0900

    feat: connect ws

--- src/server.js
+++ src/server.js
@@ -7,17 +7,23 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname+"/views");
 app.use("/public", express.static(__dirname+"/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
 // 다른 주소로 들어오면 바로 리다렉해줌
-app.get("/*", (req, res)=>res.redirect("/"));
+app.get("/*", (_, res)=>res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 
-// http랑 webSocket 둘 다 작동 시키는 것 (둘 다 돌리는 건 필수가 아님) -> same port에 서버를 돌리려고 이렇게 적음
+// http랑 webSocket 둘 다 작동 시키는 것 (둘 다 돌리는 건 필수가 아님) -> 두 프로토콜이 same port에서 작동
 const server = http.createServer(app);
 // http 서버 위에 ws 서버를 만듦
 const wss = new WebSocket.Server({server});
+
+const handleConnection = (socket) => {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
