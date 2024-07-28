9d5981b56853c013fd82a5dd2c4a6606bf749bda
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-07-28 20:34:42 +0900

    feat: redirect to home

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ app.set("views", __dirname+"/views");
 app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req, res) => res.render("home"));
 
+// 다른 주소로 들어오면 바로 리다렉해줌
+app.get("/*", (req, res)=>res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
